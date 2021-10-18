52cf4dbadfeb57cfe1fb60519528bc1b278541a9
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2021-10-18 13:45:49 -0300

    Add elbow, and silhouette analysis using the embeddings of the titles

--- exploration/clustering.py
+++ exploration/clustering.py
@@ -18,10 +18,8 @@ import numpy as np
 from adjustText import adjust_text
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
-
-from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
-#help(fasttext.FastText)
+from yellowbrick.cluster import SilhouetteVisualizer, KElbowVisualizer
 # %%
 URL = "https://www.famaf.unc.edu.ar/~nocampo043/ML_2019_challenge_dataset_preprocessed.csv"
 NOF_SAMPLES = 20000
@@ -32,16 +30,16 @@ def unsupervised_data_gen(sentences, corpus_file):
         for s in sentences:
             out.write(s + "\n")
 
-def get_embedding(vocab, model):
+def get_word_embedding(vocab, vectorize):
     return (
-        pd.DataFrame({word: model[word] for word in vocab})
+        pd.DataFrame({word: vectorize(word) for word in vocab})
         .transpose()
         .reset_index()
         .rename(columns={"index": "word"})
     )
 
-def get_embedding_2DTSNE(vocab, model):
-    embedding = get_embedding(vocab, model)
+def get_word_embedding_2DTSNE(vocab, model):
+    embedding = get_word_embedding(vocab, model)
     X = embedding.drop(columns=["word"])
     X_TSNE = TSNE(n_components=2).fit_transform(X)
     embedding_TSNE = pd.concat(
@@ -49,36 +47,57 @@ def get_embedding_2DTSNE(vocab, model):
          pd.DataFrame(X_TSNE)], axis=1)
     return embedding_TSNE
 
-def plot_vocabulary_2DTSNE(X_TSNE,
-                           vocab,
-                           ax,
-                           point_size=50,
-                           legend_size=20,
-                           tick_size=20,
-                           annotation_size=20):
+def get_sentence_embedding(sentences, vectorize):
+    return (
+        pd.DataFrame({s: vectorize(s) for s in sentences})
+        .transpose()
+        .reset_index()
+        .rename(columns={"index": "sentence"})
+    )
+
+def get_sentence_embedding_2DTSNE(sentences, vectorize):
+    embedding = get_sentence_embedding(sentences, vectorize)
+    X = embedding.drop(columns=["sentence"])
+    X_TSNE = TSNE(n_components=2).fit_transform(X)
+    embedding_TSNE = pd.concat(
+        [pd.DataFrame(sentences, columns=["sentence"]),
+         pd.DataFrame(X_TSNE)], axis=1)
+    return embedding_TSNE
+
+
+def plot_2DTSNE(X_TSNE,
+                expressions,
+                ax,
+                point_size=50,
+                legend_size=20,
+                tick_size=20,
+                annotation_size=20,
+                annotate=True):
     nof_words = len(X_TSNE)
     ax.scatter(X_TSNE[:, 0],
                X_TSNE[:, 1],
                s=point_size)
     ax.set_title("t-SNE Plot", fontsize=legend_size)
     annot_list = []
-    
-    nof_words_to_annotate = 20
-    for i in np.random.randint(nof_words, size=nof_words_to_annotate):
-        a = ax.annotate(vocab[i], (X_TSNE[i, 0], X_TSNE[i, 1]),
-                        size=annotation_size)
-        annot_list.append(a)
-    adjust_text(annot_list)
+
+    if annotate:
+        nof_words_to_annotate = 20
+        for i in np.random.randint(nof_words, size=nof_words_to_annotate):
+            a = ax.annotate(expressions[i], (X_TSNE[i, 0], X_TSNE[i, 1]),
+                            size=annotation_size)
+            annot_list.append(a)
+        adjust_text(annot_list)
+
     ax.tick_params(axis='both', which='major', labelsize=tick_size)
 
 
-def plot_vocabulary_kmeans(X_TSNE,
-                           km_model,
-                           ax,
-                           point_size=50,
-                           marker_size=2,
-                           legend_size=20,
-                           tick_size=20):
+def plot_2D_kmeans(X_TSNE,
+                   km_model,
+                   ax,
+                   point_size=50,
+                   marker_size=2,
+                   legend_size=20,
+                   tick_size=20):
     for k in range(km_model.n_clusters):
         ax.scatter(X_TSNE[km_model.labels_ == k, 0],
                    X_TSNE[km_model.labels_ == k, 1],
@@ -91,14 +110,30 @@ def plot_vocabulary_kmeans(X_TSNE,
     ax.set_title("KMeans Plot", fontsize=legend_size)
     ax.tick_params(axis='both', which='major', labelsize=tick_size)
 
+
+def plot_silhouette(X, km_model, ax, title_size=20, tick_size=20):
+    visualizer = SilhouetteVisualizer(km_model, colors='yellowbrick', ax=ax)
+    visualizer.fit(X)
+    ax.set_title("Silhoutte Plot", fontsize=title_size)
+    ax.tick_params(axis='both', which='major', labelsize=tick_size)
+
+def plot_elbow(X, estimator, metric, k_range, ax, title_size=10, tick_size=10):
+    visualizer = KElbowVisualizer(estimator(),
+                                  k=k_range,
+                                  metric=metric,
+                                  timings=False,
+                                  ax=ax)
+    visualizer.fit(X)
+    ax.set_title("Silhoutte Plot", fontsize=title_size)
+    ax.tick_params(axis='both', which='major', labelsize=tick_size)
+
+
 # %%
 df = pd.read_csv(URL)
 df = df.sample(n=NOF_SAMPLES, random_state=SEED)
-df = df.sort_values(by="category").reset_index()
-df = df.sort_values(by="language").reset_index()
 df
 # %%
-sentences = df["cleaned_title"].tolist()
+sentences = df["cleaned_title"].drop_duplicates().tolist()
 corpus_file = "titles.txt"
 # %%
 unsupervised_data_gen(sentences, corpus_file)
@@ -108,154 +143,61 @@ model = fasttext.train_unsupervised(corpus_file,
                                     lr=0.3,
                                     epoch=100,
                                     dim=100,
-                                    wordNgrams=3,
-                                    ws=3)
+                                    wordNgrams=4,
+                                    ws=4)
 # %%
-model.get_dimension(
+model.get_dimension()
 # %%
 model.get_words()[:30]
 # %%
 model.get_nearest_neighbors("barbero")
 # %%
 model.get_nearest_neighbors("cafetera")
-# %%
+# %% [markdown]
 # ## Clustering a nivel de palabra
 # %%
-# %matplotlib inline
 vocab = model.get_words()
-embedding = get_embedding(vocab, model)
-embedding_TSNE = get_embedding_2DTSNE(vocab, model)
+embedding = get_word_embedding(vocab, model.get_word_vector)
+embedding_TSNE = get_word_embedding_2DTSNE(vocab, model.get_word_vector)
 X = embedding.drop(columns=["word"]).to_numpy()
 X_TSNE = embedding_TSNE.drop(columns=["word"]).to_numpy()
 # %%
-kmeans = KMeans(n_clusters=20, algorithm='elkan')
+kmeans = KMeans(n_clusters=5)
 kmeans.fit(X)
-
+# %%
 _, (ax_kmeans, ax_tsne) = plt.subplots(1, 2, figsize=(30, 10))
-plot_vocabulary_2DTSNE(X_TSNE, vocab, ax_tsne)
-plot_vocabulary_kmeans(X_TSNE, kmeans, ax_kmeans)
+plot_2DTSNE(X_TSNE, vocab, ax_tsne)
+plot_2D_kmeans(X_TSNE, kmeans, ax_kmeans)
 ax_kmeans.grid()
 ax_tsne.grid()
-# %%
-import matplotlib.cm as cm
-
-range_n_clusters = [16, 18, 20]
-sse ={}
-for n_clusters in range_n_clusters:
-    # Create a subplot with 1 row and 2 columns
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.set_size_inches(18, 7)
-
-    # The 1st subplot is the silhouette plot
-    # The silhouette coefficient can range from -1, 1 but in this example all
-    # lie within [-0.1, 1]
-    ax1.set_xlim([-0.1, 1])
-    # The (n_clusters+1)*10 is for inserting blank space between silhouette
-    # plots of individual clusters, to demarcate them clearly.
-    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
-
-    # Initialize the clusterer with n_clusters value and a random generator
-    # seed of 10 for reproducibility.
-    clusterer = KMeans(n_clusters=n_clusters, algorithm='elkan')
-    cluster_labels = clusterer.fit_predict(X)
-    sse[n_clusters] = clusterer.inertia_
-
-    # The silhouette_score gives the average value for all the samples.
-    # This gives a perspective into the density and separation of the formed
-    # clusters
-    silhouette_avg = silhouette_score(X, cluster_labels)
-    print("Para n_clusters =", n_clusters,
-          "El silhouette_score promedio es :", silhouette_avg)
-
-    # Compute the silhouette scores for each sample
-    sample_silhouette_values = silhouette_samples(X, cluster_labels)
-
-    y_lower = 10
-    for i in range(n_clusters):
-        # Aggregate the silhouette scores for samples belonging to
-        # cluster i, and sort them
-        ith_cluster_silhouette_values = \
-            sample_silhouette_values[cluster_labels == i]
-
-        ith_cluster_silhouette_values.sort()
-
-        size_cluster_i = ith_cluster_silhouette_values.shape[0]
-        y_upper = y_lower + size_cluster_i
-
-        color = cm.nipy_spectral(float(i) / n_clusters)
-        ax1.fill_betweenx(np.arange(y_lower, y_upper),
-                          0, ith_cluster_silhouette_values,
-                          facecolor=color, edgecolor=color, alpha=0.7)
-
-        # Label the silhouette plots with their cluster numbers at the middle
-        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-        # Compute the new y_lower for next plot
-        y_lower = y_upper + 10  # 10 for the 0 samples
-
-    ax1.set_title("Visualizacion de los datos.")
-    ax1.set_xlabel("espacio de la primera caracteristica")
-    ax1.set_ylabel("espacio de la segunda caracteristica")
-
-    # The vertical line for average silhouette score of all the values
-    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
-
-    ax1.set_yticks([])  # Clear the yaxis labels / ticks
-    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-
-    # 2nd Plot showing the actual clusters formed
-    colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
-    ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30, lw=0, alpha=0.7,
-                c=colors, edgecolor='k')
-
-    # Labeling the clusters
-    centers = clusterer.cluster_centers_
-    # Draw white circles at cluster centers
-    ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
-                c="white", alpha=1, s=200, edgecolor='k')
-
-    for i, c in enumerate(centers):
-        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
-                    s=50, edgecolor='k')
-
-    ax2.set_title("Visualizacion de los datos.")
-    ax2.set_xlabel("espacio de la primera caracteristica")
-    ax2.set_ylabel("espacio de la segunda caracteristica")
-
-    plt.suptitle(("Analisis de silueta para Kmedias "
-                  "con n_clusters = %d" % n_clusters),
-                 fontsize=14, fontweight='bold')
-    #plt.savefig("kmeans_%d" % n_clusters, dpi=300)
-
-plt.show()
-
-fig, (ax1, ax2) = plt.subplots(1, 2)
-fig.set_size_inches(18, 7)
-plt.suptitle(("Metodo del codo para kmedias "),
-                 fontsize=14, fontweight='bold')
-ax1.plot(list(sse.keys()), list(sse.values()))
-ax1.set_xlabel("Numero of cluster")
-ax1.set_ylabel("Inercia")
-
-clusterer = KMeans(n_clusters=4, random_state=10)
-cluster_labels = clusterer.fit_predict(X)
-    
-colors = cm.nipy_spectral(cluster_labels.astype(float) / 4)
-ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30, lw=0, alpha=0.7,c=colors, edgecolor='k')
-
-# Labeling the clusters
-centers = clusterer.cluster_centers_
-    # Draw white circles at cluster centers
-ax2.scatter(centers[:, 0], centers[:, 1], marker='o',c="white", alpha=1, s=200, edgecolor='k')
-
-for i, c in enumerate(centers):
-    ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,s=50, edgecolor='k')
-
-ax2.set_title("Visualizacion de los datos.")
-ax2.set_xlabel("espacio de la primera caracteristica")
-ax2.set_ylabel("espacio de la segunda caracteristica")
-
-#plt.savefig("kmeans_elbow", dpi=300)
 plt.show()
 # %%
+_, (ax_silhouette, ax_kmeans) = plt.subplots(1, 2, figsize=(30, 10))
+plot_silhouette(X, kmeans, ax_silhouette)
+plot_2D_kmeans(X_TSNE, kmeans, ax_kmeans)
+ax_silhouette.grid()
+ax_kmeans.grid()
+plt.show()
+# %% [markdown]
+# ## Clustering a nivel de títulos
+# %%
+embedding = get_sentence_embedding(sentences, model.get_sentence_vector)
+embedding_TSNE = get_sentence_embedding_2DTSNE(sentences,
+                                               model.get_sentence_vector)
+X = embedding.drop(columns=["sentence"]).to_numpy()
+X_TSNE = embedding_TSNE.drop(columns=["sentence"]).to_numpy()
+# %%
+fig, ax_elbow = plt.subplots()
+k_range = (2, 20)
+plot_elbow(X, KMeans, "distortion", k_range, ax_elbow)
+plt.show()
+# %%
+kmeans = KMeans(n_clusters=11)
+kmeans.fit(X)
+# %%
+_, (ax_silhouette, ax_kmeans) = plt.subplots(1, 2, figsize=(30, 10))
+plot_silhouette(X, kmeans, ax_silhouette)
+plot_2D_kmeans(X_TSNE, kmeans, ax_kmeans)
+ax_silhouette.grid()
+ax_kmeans.grid()
+plt.show()
